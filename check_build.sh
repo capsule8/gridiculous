e3cb55d7331cfaadf0f8e5f388d7285bc5ada35e
Jim McGee <jim@capsule8.com>
2019-03-04 14:58:35 -0600

    improving script feedback

--- check_build.sh
+++ check_build.sh
@@ -1,2 +1,8 @@
 yarn build
-[[ $(git status --short) ]] && exit 1
+if [[ $(git status --short) ]]
+then
+  echo -e '\n\nERROR: Built library is out of date. Run `yarn build` and commit.'
+  exit 1
+else
+  exit 0
+fi
