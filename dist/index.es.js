c3ca9ecef988a1d88290f4483965de9fa28faed1
Jim McGee <jim@capsule8.com>
2019-04-08 13:05:03 -0400

    bumping versions

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { createContext, useRef, useState, useEffect, useCallback, createElement, Fragment, useContext, useMemo, memo, forwardRef } from 'react';
 import { Draggable, DragDropContext, Droppable } from 'react-beautiful-dnd';
 
@@ -2807,6 +2808,7 @@ var pick = flatRest(function(object, paths) {
 
 const GridNodeContext = createContext(null);
 const ClassNamesContext = createContext({});
+//# sourceMappingURL=context.js.map
 
 function createCommonjsModule(fn, module) {
 	return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -2916,6 +2918,7 @@ function useIntersectionObserver({ root, rootMargin, target, isEnabled, }) {
     }, [isEnabled, root, rootMargin, target]);
     return isIntersecting;
 }
+//# sourceMappingURL=useIntersectionObserver.js.map
 
 function ColumnIntersectionObserver({ columnIndex, dataLength, onIsIntersectingChange, isEnabled, }) {
     const gridNode = useContext(GridNodeContext);
@@ -2959,6 +2962,7 @@ function useColumnsIntersectionObserver({ columns, dataLength, isEnabled, }) {
         createElement(Fragment, null, columnVisibility.map((_c, columnIndex) => (createElement(ColumnIntersectionObserver, { columnIndex: columnIndex, dataLength: dataLength, isEnabled: isEnabled, key: columns[columnIndex].key, onIsIntersectingChange: handleColumnIntersectingChange })))),
     ];
 }
+//# sourceMappingURL=useColumnIntersectionObserver.js.map
 
 /** `Object#toString` result references. */
 var numberTag$2 = '[object Number]';
@@ -3015,6 +3019,7 @@ function columnsToGridTemplate(columns, defaultColumnMinWidth, override) {
     })
         .join(' ');
 }
+//# sourceMappingURL=columnsToGridTemplate.js.map
 
 /** Used to compose bitmasks for value comparisons. */
 var COMPARE_PARTIAL_FLAG$4 = 1,
@@ -3583,6 +3588,7 @@ var sortBy = baseRest(function(collection, iteratees) {
 function sortColumns(columns, columnOrder) {
     return sortBy(columns, ({ key }) => columnOrder.indexOf(key));
 }
+//# sourceMappingURL=sortColumns.js.map
 
 function spliceColumnOrder(columnOrder, sourceIndex, destinationIndex) {
     const newColumnOrder = [...columnOrder];
@@ -3590,6 +3596,7 @@ function spliceColumnOrder(columnOrder, sourceIndex, destinationIndex) {
     newColumnOrder.splice(destinationIndex, 0, removed);
     return newColumnOrder;
 }
+//# sourceMappingURL=spliceColumnOrder.js.map
 
 function useTrackCells({ headerCellRefs, trackingCellRefs, }) {
     return useCallback((columns) => {
@@ -3605,6 +3612,7 @@ function useTrackCells({ headerCellRefs, trackingCellRefs, }) {
         });
     }, [headerCellRefs, trackingCellRefs]);
 }
+//# sourceMappingURL=useTrackCells.js.map
 
 function useDragDrop({ columns, defaultColumnMinWidth, isColumnDragDisabled, onColumnsOrderChange, }) {
     const gridNode = useContext(GridNodeContext);
@@ -3707,6 +3715,7 @@ function useDragDrop({ columns, defaultColumnMinWidth, isColumnDragDisabled, onC
         trackingCellRefs,
     };
 }
+//# sourceMappingURL=useDragDrop.js.map
 
 function usePaneScrollState() {
     const gridNode = useContext(GridNodeContext);
@@ -3749,6 +3758,7 @@ function usePaneScrollState() {
     }, [gridNode, checkScroll]);
     return { canScrollLeft, canScrollRight };
 }
+//# sourceMappingURL=usePaneScrollState.js.map
 
 function applyColumnWidthDefaults(column) {
     const { defaultWidth, width } = column;
@@ -3778,6 +3788,7 @@ function applyColumnMinWidth(column) {
     }
     return column;
 }
+//# sourceMappingURL=columnWidthHelpers.js.map
 
 /**
  * Checks if `value` is `undefined`.
@@ -3887,6 +3898,7 @@ function ResizeDragHandle({ isHidden, maxWidth = 1000, minWidth = 50, onWidthCha
         }) },
         createElement("div", { className: styles.ExtraHandle })));
 }
+//# sourceMappingURL=ResizeDragHandle.js.map
 
 var css$1 = ".HeaderCell_HeaderCell__lVi2L {\n  box-sizing: border-box;\n  display: flex;\n  align-items: center;\n  position: relative;\n  grid-row: 1;\n  border: 1px solid rgba(0, 0, 0, 0);\n  opacity: 1;\n  transition: 150 opacity ease-out, 250ms box-shadow ease-out; }\n  .HeaderCell_HeaderCell__lVi2L:hover {\n    transition: 250ms box-shadow ease-out;\n    border-right: 1px solid #e6e6e6;\n    border-left: 1px solid #e6e6e6; }\n  .HeaderCell_HeaderCell__lVi2L.HeaderCell_isAnyDragging__3KJwT:not(.HeaderCell_notResizable__2RBMh) {\n    background-color: white;\n    border-right: 1px solid #e6e6e6; }\n  .HeaderCell_HeaderCell__lVi2L.HeaderCell_notResizable__2RBMh:hover {\n    border: 1px solid rgba(0, 0, 0, 0); }\n  .HeaderCell_HeaderCell__lVi2L.HeaderCell_isDragging__32Ss7 {\n    box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px;\n    transition: 150ms opacity ease-in, 300ms box-shadow ease-in;\n    opacity: 0.7; }\n\n.HeaderCell_LabelContainer__hXbBQ {\n  width: 100%;\n  height: 100%;\n  padding: 0 16px;\n  overflow: hidden;\n  text-overflow: ellipsis;\n  white-space: nowrap;\n  display: flex;\n  align-items: center; }\n\n.HeaderCell_Label__SrYof {\n  width: 100%;\n  overflow-x: hidden;\n  text-overflow: ellipsis;\n  white-space: nowrap; }\n";
 var styles$1 = {"HeaderCell":"HeaderCell_HeaderCell__lVi2L","isAnyDragging":"HeaderCell_isAnyDragging__3KJwT","notResizable":"HeaderCell_notResizable__2RBMh","isDragging":"HeaderCell_isDragging__32Ss7","LabelContainer":"HeaderCell_LabelContainer__hXbBQ","Label":"HeaderCell_Label__SrYof"};
@@ -3924,6 +3936,7 @@ function HeaderCellWrapped({ column: { key, width, label, minWidth, name, notRes
             createElement("div", { className: styles$1.Label }, displayLabel)),
         onWidthChangeEnd && !notResizable && (createElement(ResizeDragHandle, { isHidden: isAnyDragging, minWidth: minWidth, shouldMoveLeftToAvoidOverflow: isLastChild, targetRef: ref, onWidthChange: handleTransientWidthChange, onWidthChangeEnd: handleWidthChange }))));
 }
+//# sourceMappingURL=HeaderCellWrapped.js.map
 
 function HeaderCell(props) {
     const { column, columnIndex, draggingKey, headerCellRefs, isDragDisabled, isLastChild, onWidthChange, onWidthChangeEnd, } = props;
@@ -3955,6 +3968,7 @@ function getAxisLockedTransform(style) {
     }
     return `${parts[0]}, 0)`;
 }
+//# sourceMappingURL=HeaderCell.js.map
 
 var css$2 = ".Header_Header__1I5CE {\n  border-bottom: 1px solid #e6e6e6;\n  grid-column: 1 / -1;\n  display: flex;\n  font-weight: bold; }\n\n.Header_isAnyDragging__1wGte {\n  background-color: #e6e6e6; }\n";
 var styles$2 = {"Header":"Header_Header__1I5CE","isAnyDragging":"Header_isAnyDragging__1wGte"};
@@ -4002,6 +4016,7 @@ function Header({ columns: rawColumns, defaultColumnMinWidth, draggingKey, heade
                 }
             } })))));
 }
+//# sourceMappingURL=Header.js.map
 
 function useHoverState() {
     const [isHovering, setIsHovering] = useState(false);
@@ -4019,6 +4034,7 @@ function useHoverState() {
         },
     ];
 }
+//# sourceMappingURL=useHoverState.js.map
 
 var css$3 = ".Cell_Cell__1-3zV {\n  background-color: inherit;\n  box-sizing: border-box;\n  padding: 0 16px;\n  color: #494949;\n  display: flex;\n  align-items: center;\n  position: relative;\n  border-bottom: 1px solid #e6e6e6;\n  transition: 200ms color ease-out; }\n  .Cell_Cell__1-3zV.Cell_isLastRow__1vtWp {\n    border-bottom: none; }\n\n.Cell_Content__sRZrE {\n  overflow-x: hidden;\n  text-overflow: ellipsis;\n  white-space: nowrap; }\n\n.Cell_isDragging__1GXEv {\n  color: gray;\n  transition: 200ms color ease-in; }\n";
 var styles$3 = {"Cell":"Cell_Cell__1-3zV","isLastRow":"Cell_isLastRow__1vtWp","Content":"Cell_Content__sRZrE","isDragging":"Cell_isDragging__1GXEv"};
@@ -4046,6 +4062,7 @@ const Cell = memo(forwardRef(({ column, columnIndex, datum, isColumnVisible, isD
         } },
         createElement("div", { className: styles$3.Content }, value)));
 }));
+//# sourceMappingURL=Cell.js.map
 
 var css$4 = ".RowOverlay_RowOverlay__2YnzW {\n  position: absolute;\n  top: 1px;\n  bottom: 1px;\n  align-items: center;\n  pointer-events: none; }\n";
 var styles$4 = {"RowOverlay":"RowOverlay_RowOverlay__2YnzW"};
@@ -4118,6 +4135,7 @@ function RowOverlay({ children, columnsLength, isHoveringRow, rowIndex, }) {
             gridRowStart: rowIndex + 2,
         } }, children));
 }
+//# sourceMappingURL=RowOverlay.js.map
 
 var css$5 = ".Row_Cell__Wkai2 {\n  background-color: inherit;\n  box-sizing: border-box;\n  padding: 0 16px;\n  color: #494949;\n  display: flex;\n  align-items: center;\n  position: relative;\n  border-bottom: 1px solid #e6e6e6;\n  transition: 200ms color ease-out; }\n  .Row_Cell__Wkai2.Row_isLastRow__kcUhG {\n    border-bottom: none; }\n\n.Row_Content__2erQp {\n  overflow-x: hidden;\n  text-overflow: ellipsis;\n  white-space: nowrap; }\n\n.Row_isDragging__2r8Gs {\n  color: gray;\n  transition: 200ms color ease-in; }\n\n.Row_Row__3uXq8 {\n  display: contents; }\n  .Row_Row__3uXq8:hover {\n    background-color: #fafafa; }\n  .Row_Row__3uXq8.Row_isClickable__3EDnY {\n    cursor: pointer; }\n\n.Row_NullCell__2XBZD {\n  background-color: inherit;\n  width: 100%;\n  height: 100%;\n  border-bottom: 1px solid #e6e6e6; }\n";
 var styles$5 = {"Cell":"Row_Cell__Wkai2","isLastRow":"Row_isLastRow__kcUhG","Content":"Row_Content__2erQp","isDragging":"Row_isDragging__2r8Gs","Row":"Row_Row__3uXq8","isClickable":"Row_isClickable__3EDnY","NullCell":"Row_NullCell__2XBZD"};
@@ -4160,6 +4178,7 @@ const Row = memo(({ cellRefs, columns, columnVisibility, datum, draggingKey, isL
             rowIndex,
         })))));
 });
+//# sourceMappingURL=Row.js.map
 
 var css$6 = ".Grid_GridPane__3SgbV {\n  color: #494949;\n  position: relative;\n  width: 100%;\n  margin-top: -100vh;\n  pointer-events: none; }\n\n.Grid_Grid__2Cmk2 {\n  text-align: left;\n  overflow-x: auto;\n  overflow-y: hidden;\n  display: grid;\n  position: relative;\n  background-color: white;\n  margin-top: 100vh;\n  pointer-events: all;\n  border: 1px solid #e6e6e6;\n  transition: 160ms box-shadow ease-out;\n  padding-left: 4px;\n  padding-right: 4px; }\n  .Grid_Grid__2Cmk2 .Grid_HeaderCell__1MH3g {\n    display: none; }\n  .Grid_Grid__2Cmk2.Grid_canScrollRight__25Eu4 {\n    box-shadow: inset -8px 0px 8px -8px rgba(0, 0, 0, 0.1); }\n  .Grid_Grid__2Cmk2.Grid_canScrollLeft__3SlfR {\n    box-shadow: inset 8px 0px 8px -8px rgba(0, 0, 0, 0.1); }\n  .Grid_Grid__2Cmk2.Grid_canScrollLeft__3SlfR.Grid_canScrollRight__25Eu4 {\n    box-shadow: inset -8px 0px 8px -8px rgba(0, 0, 0, 0.1), inset 8px 0px 8px -8px rgba(0, 0, 0, 0.1); }\n\n.Grid_canScrollLeft__3SlfR,\n.Grid_canScrollRight__25Eu4 {\n  transition: 160ms box-shadow ease-in; }\n";
 var styles$6 = {"GridPane":"Grid_GridPane__3SgbV","Grid":"Grid_Grid__2Cmk2","HeaderCell":"Grid_HeaderCell__1MH3g","canScrollRight":"Grid_canScrollRight__25Eu4","canScrollLeft":"Grid_canScrollLeft__3SlfR"};
@@ -4203,6 +4222,7 @@ const GridWrapped = forwardRef(({ columns: rawColumns, data, defaultColumnMinWid
                 data.map((datum, rowIndex) => (createElement(Row, { cellRefs: cellRefs, columns: columns, columnVisibility: columnVisibility, datum: datum, draggingKey: draggingKey, isLastRow: rowIndex === data.length - 1, isSelected: Boolean(selectedRowIndexes && selectedRowIndexes.has(rowIndex)), key: rowKeyAccessor(datum), onClick: onRowClick, rowIndex: rowIndex, rowOverlay: rowOverlay }))),
                 observerComponent))))));
 });
+//# sourceMappingURL=GridWrapped.js.map
 
 const Grid = memo(forwardRef((props, externalRef) => {
     const gridRef = useRef(null);
@@ -4226,6 +4246,7 @@ const Grid = memo(forwardRef((props, externalRef) => {
     }
     return true;
 });
+console.log({ React });
 
 function useColumnOrderState(columns) {
     const [columnOrder, setColumnOrder] = useState(columns.map(({ key }) => key));
@@ -4234,6 +4255,9 @@ function useColumnOrderState(columns) {
     }, [columnOrder]);
     return [columnOrder, onColumnOrderChange];
 }
+//# sourceMappingURL=useColumnOrderState.js.map
+
+//# sourceMappingURL=index.js.map
 
 export { Grid, useColumnOrderState, sortColumns };
 //# sourceMappingURL=index.es.js.map
